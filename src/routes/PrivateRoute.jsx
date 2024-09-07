3392800d21957dc1399c5501500978341f539719
indiakka <78439130+indiakka@user.noreply.github.com>
2024-09-07 19:31:47 +0200

    change routes and create private routes

---
+++ src/routes/PrivateRoute.jsx
@@ -0,0 +1,11 @@
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+import React from "react";
+
+const PrivateRoute = ({ children }) => {
+  const { isAuthenticated } = useAuth(); // Verificar autenticación desde el contexto
+
+  return isAuthenticated ? children : <Navigate to="/login" />;
+};
+
+export default PrivateRoute;
