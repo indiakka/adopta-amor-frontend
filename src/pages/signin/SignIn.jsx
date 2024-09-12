75531b0c7b84105cc5a44be9495bd2283601abd6
indiakka <78439130+indiakka@user.noreply.github.com>
2024-09-12 09:25:03 +0200

    popups signin working ok

--- src/pages/signin/SignIn.jsx
+++ src/pages/signin/SignIn.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import "./signin.css"; // Archivo CSS para estilos
 import Popup from "../../components/popups/Popups";
@@ -10,47 +10,81 @@ const SignIn = () => {
   const [password, setPassword] = useState("");
   const [isPopupOpen, setIsPopupOpen] = useState(false);
   const [popupMessage, setPopupMessage] = useState("");
+  const [nameError, setNameError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+
   const navigate = useNavigate();
 
   const closePopup = () => setIsPopupOpen(false);
 
-  const onSubmit = async (event) => {
+  const handleName = (e) => {
+    setNombre(e.target.value);
+    if (e.target.value) {
+      setNameError(false);
+    }
+  };
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+    if (e.target.value) {
+      setEmailError(false);
+    }
+  };
+
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+    if (e.target.value) {
+      setPasswordError(false);
+    }
+  };
+
+  const handleSubmit = async (event) => {
     event.preventDefault();
 
-    const datosRegistro = {
-      nombre,
-      email,
-      password,
-    };
+    if (!nombre || !email || !password) {
+      setNameError(!nombre);
+      setEmailError(!email);
+      setPasswordError(!password);
+      return;
+    }
 
     try {
-      await axios.post("/api/registro", datosRegistro);
+      const response = await axios.post("http://localhost:4001/auth/register", {
+        email,
+        password,
+        name: nombre,
+      });
 
-      setPopupMessage("Registro exitoso. Redirigiendo...");
-      setIsPopupOpen(true);
-
-      setTimeout(() => {
-        navigate("/login");
-      }, 2000);
+      if (response.data) {
+        setPopupMessage("Registro exitoso. Redirigiendo al login...");
+        setIsPopupOpen(true);
+        setTimeout(() => navigate("/login"), 2000);
+      } else {
+        setPopupMessage("Error en el servidor. Inténtalo nuevamente.");
+        setIsPopupOpen(true);
+      }
     } catch (error) {
-      setPopupMessage("Hubo un problema con el registro.");
+      console.error("Error en el servidor:", error);
+      setPopupMessage("Error en el servidor. Inténtalo nuevamente.");
       setIsPopupOpen(true);
     }
   };
 
   return (
     <div className="container--signin">
-      <form onSubmit={onSubmit} className="form">
+      <form onSubmit={handleSubmit} className="form">
         <div className="form-group">
           <label htmlFor="nombre">Nombre</label>
           <input
             type="text"
             id="nombre"
             value={nombre}
-            onChange={(e) => setNombre(e.target.value)}
+            onChange={handleName}
             placeholder="Ingresa tu nombre"
             required
           />
+          {nameError && <p className="error-text">Nombre requerido</p>}
         </div>
         <div className="form-group">
           <label htmlFor="email">Correo electrónico</label>
@@ -58,10 +92,11 @@ const SignIn = () => {
             type="email"
             id="email"
             value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            onChange={handleEmail}
             placeholder="Ingresa tu correo electrónico"
             required
           />
+          {emailError && <p className="error-text">Email requerido</p>}
         </div>
         <div className="form-group">
           <label htmlFor="password">Contraseña</label>
@@ -69,19 +104,21 @@ const SignIn = () => {
             type="password"
             id="password"
             value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            onChange={handlePassword}
             placeholder="Ingresa tu contraseña"
             required
           />
+          {passwordError && <p className="error-text">Contraseña requerida</p>}
         </div>
         <button type="submit" className="button-signin">
           Registrarse
         </button>
       </form>
       <div className="signin-footer">
-        <p>
-          ¿Ya tienes cuenta? <a href="/login">Accede aquí</a>
-        </p>
+        ¿Ya tienes cuenta? Accede{" "}
+        <Link to="/login" className="link">
+          aquí
+        </Link>
       </div>
       {isPopupOpen && (
         <Popup
