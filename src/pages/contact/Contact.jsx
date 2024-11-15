e24bb3fe8a542d2b08aa970de8cb262d6c693adc
indiakka <78439130+indiakka@user.noreply.github.com>
2024-11-15 17:37:16 +0100

    connect the form with emailjs

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,27 +1,96 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
 import "./contact.css";
+import Popup from "../../components/popups/Popups";
 
 const Contact = () => {
   const [mostrarNotificacion, setMostrarNotificacion] = useState(false);
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    number: "",
+    message: "",
+  });
+
+  const form = useRef();
+
+  const handleChange = (e) => {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value,
+    });
+  };
   const handleSubmit = (e) => {
     e.preventDefault();
+    console.log(form.current);
+
     setMostrarNotificacion(true);
+
+    emailjs
+      .sendForm(
+        import.meta.env.VITE_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+        form.current,
+        import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+      )
+
+      .then(
+        (result) => {
+          console.log(result.text);
+          setMostrarNotificacion(true);
+          console.log(form.current);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
   };
   return (
     <div className="formContainerContacto">
-      <form className="form" onSubmit={handleSubmit}>
+      <form ref={form} className="form contact-form" onSubmit={handleSubmit}>
         <h1>Formulario de Contacto</h1>
         <p>
-          <b>Rellene los siguientes campos: </b>
+          <b>Rellene los siguientes campos </b>
         </p>
         <div className="container--input--divs">
-          <input type="text" placeholder="Nombre completo*" />
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={values.name}
+            onChange={handleChange}
+            placeholder="Nombre completo*"
+          />
         </div>
         <div className="container--input--divs">
-          <input type="text" placeholder="Correo electrónico*" />
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={values.email}
+            onChange={handleChange}
+            placeholder="Correo electrónico*"
+          />
         </div>
         <div className="container--input--divs">
-          <input type="text" placeholder="Número de contacto*"  />
+          <input
+            type="number"
+            id="number"
+            name="number"
+            value={values.number}
+            onChange={handleChange}
+            placeholder="Número de contacto*"
+          />
+        </div>
+        <div className="container--input--divs email-message">
+          <input
+            type="text"
+            id="message"
+            name="message"
+            onChange={handleChange}
+            value={values.message}
+            placeholder="Mensaje*"
+          />
         </div>
         <div
           className={`ocultarNotificacion ${
