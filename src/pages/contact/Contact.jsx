699cbac916bb62bea71512d5f160f6f04564e971
indiakka <78439130+indiakka@user.noreply.github.com>
2025-02-17 16:14:37 +0100

    changed the popup for an alert with sweet alert2, and added  validation to the form

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,16 +1,22 @@
 import React, { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
 import "./contact.css";
-import Popup from "../../components/popups/Popups";
+import Alerta from "../../components/alerta/Alerta";
+import { useNavigate } from "react-router";
 
 const Contact = () => {
-  const [mostrarNotificacion, setMostrarNotificacion] = useState(false);
+  const [mostrarNotificacion, setMostrarNotificacion] = useState({
+    isOpen: false,
+    title: "",
+    icon: "",
+  });
   const [values, setValues] = useState({
     name: "",
     email: "",
     number: "",
     message: "",
   });
+  const navigate = useNavigate();
 
   const form = useRef();
 
@@ -20,11 +26,36 @@ const Contact = () => {
       [e.target.name]: e.target.value,
     });
   };
+
+  const validarFormulario = () => {
+    if (!values.name || !values.email || !values.message) {
+      setMostrarNotificacion({
+        isOpen: true,
+        title: "Error",
+        text: "Por favor, completa todos los campos requeridos",
+        icon: "error",
+      });
+      return false;
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(values.email)) {
+      setMostrarNotificacion({
+        isOpen: true,
+        title: "Error",
+        text: "Por favor, introduce un email válido.",
+        icon: "error",
+      });
+      return false;
+    }
+    return true;
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
-
-    setMostrarNotificacion(true);
-
+    if (!validarFormulario()) {
+      return;
+    }
     emailjs
       .sendForm(
         import.meta.env.VITE_EMAILJS_SERVICE_ID,
@@ -35,28 +66,44 @@ const Contact = () => {
 
       .then(
         (result) => {
-          setMostrarNotificacion( true );
-            setValues({
-              name: "",
-              email: "",
-              number: "",
-              message: "",
-            });
-          form.current.reset()
+          setValues({
+            name: "",
+            email: "",
+            number: "",
+            message: "",
+          });
+          setMostrarNotificacion({
+            isOpen: true,
+            title:
+              result.text === "OK"
+                ? "¡Gracias!¡Nos pondremos en contacto muy pronto!"
+                : "Mensaje no envíado, comprueba que los campos no estén vacíos",
+            icon: result.text === "OK" ? "success" : "error",
+            onConfirm: () => {
+              form.current.reset();
+              navigate("/adoptar");
+            },
+          });
         },
         (error) => {
-          console.log(error.text);
+          setMostrarNotificacion({
+            isOpen: true,
+            title: "Error",
+            text: error.text || error.message || "Ocurrió un error inesperado",
+            icon: "error",
+          });
         }
-
       );
   };
   return (
     <div className="formContainerContacto">
       <form ref={form} className="form contact-form " onSubmit={handleSubmit}>
         <h1>Formulario de Contacto</h1>
-        <p>
           <b>Rellene los siguientes campos </b>
-        </p>
+          <p>(Los campos con * son obligatorios)</p>
+        <label className="label-contact" htmlFor="name">
+          Nombre y Apellido{" "}
+        </label>
         <div className="container--input--divs contact-form">
           <input
             type="text"
@@ -64,9 +111,12 @@ const Contact = () => {
             name="name"
             value={values.name}
             onChange={handleChange}
-            placeholder="Nombre completo*"
+            placeholder="Nombre completo"
           />
-        </div>
+        </div>{" "}
+        <label className="label-contact" htmlFor="email">
+          Email{" "}
+        </label>
         <div className="container--input--divs contact-form">
           <input
             type="email"
@@ -74,9 +124,10 @@ const Contact = () => {
             name="email"
             value={values.email}
             onChange={handleChange}
-            placeholder="Correo electrónico*"
+            placeholder="Correo electrónico"
           />
         </div>
+        <label htmlFor="number">Número de teléfono </label>
         <div className="container--input--divs contact-form">
           <input
             type="number"
@@ -84,9 +135,12 @@ const Contact = () => {
             name="number"
             value={values.number}
             onChange={handleChange}
-            placeholder="Número de contacto*"
+            placeholder="Número de contacto"
           />
         </div>
+        <label className="label-contact" htmlFor="message">
+          Mensaje{" "}
+        </label>
         <div className="container--input--divs  contact-form email-message">
           <input
             type="text"
@@ -94,15 +148,15 @@ const Contact = () => {
             name="message"
             onChange={handleChange}
             value={values.message}
-            placeholder="Mensaje*"
+            placeholder="Mensaje"
           />
         </div>
         <div
           className={`ocultarNotificacion ${
-            mostrarNotificacion ? "mostrarNotificacion" : ""
+            mostrarNotificacion.isOpen ? "mostrarNotificacion" : ""
           }`}
         >
-          <Popup>¡Gracias!¡Nos pondremos en contacto muy pronto!</Popup>
+          {mostrarNotificacion.isOpen && <Alerta {...mostrarNotificacion} />}
         </div>
         <button className="button-adopta button--conoceme" type="submit">
           Enviar
