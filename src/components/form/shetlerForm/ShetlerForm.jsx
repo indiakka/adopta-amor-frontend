bbdc73c966e64a9774c3a534c7f68f68fe14a3e0
indiakka <78439130+indiakka@user.noreply.github.com>
2025-02-22 18:45:20 +0100

    created charge saved animals function into the form

--- src/components/form/shetlerForm/ShetlerForm.jsx
+++ src/components/form/shetlerForm/ShetlerForm.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
+import React from "react";
 import { useForm } from "react-hook-form";
+import {animalesGuardados} from "../../shelter/Shelter";
 
 const ShelterForm = () => {
   const {
@@ -15,33 +17,27 @@ const ShelterForm = () => {
     setOnShelter(animalesGuardados);
   }, []);
 
- 
-
   return (
-    <form onSubmit={handleSubmit( onSubmit )}>
-      <div>{
- onShelter.map( ( animal ) =>
-  {
-    <img src={animal.imagen} />,
-        animal.id,
-        defaultChecked ={true}
-      
-  })}
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        {onShelter.map((animal) => (
+          <div key={animal.id}>
+            <img src={animal.imagen} />
+            <input
+              type="checkbox"
+              defaultChecked={true}
+              {...register(`checked.${animal.id}`, {
+                required: "Algun animal debe estar seleccionado",
+              })}
+              aria-invalid={errors.checked?.[animal.id] ? "true" : "false"}
+            />
+            {errors.checked?.[animal.id] && (
+              <p role="alert">{errors.checked?.[animal.id].message}</p>
+            )}
+          </div>
+        ))}
       </div>
-      <input
-        {...register("firstName", { required: true })}
-        aria-invalid={errors.firstName ? "true" : "false"}
-      />
-      {errors.firstName?.type === "required" && (
-        <p role="alert">First name is required</p>
-      )}
-
-      <input
-        {...register("mail", { required: "Email Address is required" })}
-        aria-invalid={errors.mail ? "true" : "false"}
-      />
-      {errors.mail && <p role="alert">{errors.mail.message}</p>}
-
+    
       <input type="submit" />
     </form>
   );
